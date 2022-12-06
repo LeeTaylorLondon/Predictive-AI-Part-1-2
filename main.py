88e5bf556c24bd0c6103eb8af977e04159b1b897
Lee Taylor <lees_taylor_18@yahoo.com>
2022-12-06 23:01:49 +0000

    Made MICE imputer, validation required

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 # Author: Lee Taylor, ST Number: 190211479
 from sklearn.impute import KNNImputer
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 import pandas as pd
+import numpy as np
 
 
 def printdf(df):
     for col in df: print(col)
 
+
+# Configure pandas display
 pd.set_option('display.max_columns', None)
 
+# > Read datasets
 mar_data = pd.read_csv("houses_0.5_MAR.csv")
 ful_data = pd.read_csv("houses.csv")
 all_data = [mar_data, ful_data]
 
+# > Create correlation matrices
 md_corr = mar_data.corr()
 fd_corr = ful_data.corr()
 al_corr = [md_corr, fd_corr]
 
-imputer    = KNNImputer()  # Default nn=5
-mar_filled = imputer.fit_transform(mar_data)
-printdf(mar_filled)
-print("\n...\n")
-printdf(mar_data)
+# > Fill missing data KNN
+imputer        = KNNImputer()  # Default nn=5
+mar_filled_knn = imputer.fit_transform(mar_data)
+
+# > Fill missing data MICE
+mar_filled_mice = mar_data.__copy__()
+lr  = LinearRegression()
+imp = IterativeImputer(estimator=lr, verbose=2, max_iter=30, tol=1e-10,
+                       imputation_order='roman')
+imp.fit(mar_data)
+imp.transform(mar_filled_mice)
+
+# > Inspect datasets
+# printdf(mar_filled)
+# print("\n...\n")
+# printdf(mar_data)
+
 
 if __name__ == '__main__':
+    print('\n...\n')
+    print(f"data.shape      = {mar_data.shape}")
+    print(f"data_knn.shape  = {mar_filled_knn.shape}")
+    print(f"data_mice.shape = {mar_filled_mice.shape}")
     pass
