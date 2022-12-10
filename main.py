e8d5150b54e36979f715287f8a70ccab05e398ee
Lee Taylor <lees_taylor_18@yahoo.com>
2022-12-10 15:09:45 +0000

    Minor code changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Author: Lee Taylor, ST Number: 190211479
-import numpy    as np
-import pandas   as pd
+import numpy                    as np
+import pandas                   as pd
 from sklearn.impute             import KNNImputer
 from sklearn.linear_model       import LinearRegression
 from sklearn.model_selection    import train_test_split, KFold
@@ -9,7 +9,7 @@ from sklearn.impute             import IterativeImputer
 from sklearn.metrics            import r2_score,  mean_squared_error
 
 
-# Configure pandas display
+# > Configure pandas display
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
@@ -18,7 +18,7 @@ mar_data = pd.read_csv("houses_0.5_MAR.csv")
 ful_data = pd.read_csv("houses.csv")
 all_data = [mar_data, ful_data]
 
-# > Create correlation matrices
+# # > Create correlation matrices
 # md_corr = mar_data.corr()
 # fd_corr = ful_data.corr()
 # al_corr = [md_corr, fd_corr]
@@ -26,7 +26,7 @@ all_data = [mar_data, ful_data]
 # > Fill missing data KNN
 imputer        = KNNImputer()  # Default nn=5
 mar_filled_knn = imputer.fit_transform(mar_data)
-del imputer  # Prevent re-using imputer in next section
+del imputer  # Prevent re-using incorrect imputer in next section
 
 # > Fill missing data MICE
 mar_filled_mice = mar_data.__copy__()
@@ -44,8 +44,8 @@ imputed_data = [pd.DataFrame(mar_filled_knn, columns=("index", "median_house_val
                                                       "total_rooms","total_bedrooms","population","households",
                                                       "latitude","longitude"))]
 
-# > Debug
-print(f"{type(imputed_data[0])}\n{imputed_data[0].columns}")
+# # > Debug
+# print(f"{type(imputed_data[0])}\n{imputed_data[0].columns}")
 
 def xtraintest(imputed_dataset, target='median_house_value', debug=False):
     """ Passed an imputed dataset, training and testing datasets are returned """
@@ -67,32 +67,34 @@ def traintestmodel(tup, iname):
     # Train model
     clf.fit(X_train.values, y_train_true.values)
     y_test_pred = clf.predict(X_test.values)
+    print(f"-------------------------------")
     print(f"{iname}-Imputed Dataset Results:")
-    print("RMSE: {0:.3}".format(mean_squared_error(y_test_true, y_test_pred)))
+    print("MSE: {0:.3}".format(mean_squared_error(y_test_true, y_test_pred)))
     print("R^2: {0:.2}".format(r2_score(y_test_true, y_test_pred)))
-    print()
+    print(f"-------------------------------")
+    return clf
 
 
 if __name__ == '__main__':
     # > Train regression models on the imputed datasets respectively
-    traintestmodel(xtraintest(imputed_data[0]), "KNN")
-    traintestmodel(xtraintest(imputed_data[1]), "MICE")
+    m1 = traintestmodel(xtraintest(imputed_data[0]), "KNN")
+    m2 = traintestmodel(xtraintest(imputed_data[1]), "MICE")
 
-    # > Debug information
+    # # > Debug information
     # print('\n...\n')
     # print(f"data.shape      = {mar_data.shape}")
     # print(f"data_knn.shape  = {mar_filled_knn.shape}")
     # print(f"data_mice.shape = {mar_filled_mice.shape}")
 
     # > Debug information
-    # > (KNN-Imputed) Train & Test dataset
+    # # > (KNN-Imputed) Train & Test dataset
     # X_train, X_test = xtraintest(imputed_data[0])
     # print(f"{X_train.shape} {X_test.shape}")
 
-    # > (MICE-Imputed) Train & Test dataset
+    # # > (MICE-Imputed) Train & Test dataset
     # X_train, X_test = xtraintest(imputed_data[1])
     # print(f"{X_train.shape} {X_test.shape}")
 
-    # > Debug info about the original complete dataset
+    # # > Debug info about the original complete dataset
     # print(f"{type(ful_data)}\n{ful_data.columns}")
     pass
